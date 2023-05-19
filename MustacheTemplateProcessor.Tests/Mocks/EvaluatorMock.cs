ffb3706d90f69956091221028696f239733578f0
Vladislav <axid1@mail.ru>
2023-05-19 14:47:05 +0300

    remove unused methods

--- MustacheTemplateProcessor.Tests/Mocks/EvaluatorMock.cs
+++ MustacheTemplateProcessor.Tests/Mocks/EvaluatorMock.cs
@@ -7,12 +7,6 @@ namespace MustacheTemplateProcessor.Tests.Mocks
 {
     public class EvaluatorMock : IEvaluator
     {
-        public T Eval<T>(string expression, Dictionary<string, object> context)
-        {
-            var evaluator = new FormulaEvaluator(context);
-            return evaluator.Eval<T>(expression);
-        }
-
         public object Eval(string expression, Dictionary<string, object> context)
         {
             var evaluator = new FormulaEvaluator(context);
@@ -23,11 +17,6 @@ namespace MustacheTemplateProcessor.Tests.Mocks
     public class EvaluatorMock2 : IEvaluator
     {
         private readonly FormulaEvaluator _formulaEvaluator = new FormulaEvaluator();
-        public T Eval<T>(string expression, Dictionary<string, object> context)
-        {
-            return _formulaEvaluator.Eval<T>(expression);
-        }
-
         public object Eval(string expression, Dictionary<string, object> context)
         {
             return _formulaEvaluator.Eval(expression, context);
